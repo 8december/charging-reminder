b68f2f13b28e2097241f35ee0ed9e90d452e85bb
8december <asheeshjohari8d@egmail.com>
2021-08-22 01:48:41 +0530

    i stupidely maked the charging value 80

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ while swi:                                  #infinit while loop to keep the prog
 
     time.sleep(1)
 
-    if percent == 80 :
+    if percent == 21 :
         Mbox("Your batter is "+ str(battery.percent) +" %  plugin the charging !", "HAHAHA !", 4096)   #pop up box
 
     # print('running')
